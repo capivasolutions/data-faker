5abf24b0997fd1d463262c3cd7b8252fbfb5371a
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2023-07-12 21:42:14 -0400

    feat: integration with backend to create fake transaction in database

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from time import sleep
 from faker import Faker
+from service import Service
 
 
 def main():
     faker = Faker()
+    service = Service()
     while True:
-        _transaction = faker.generate_transaction()
+        transaction = faker.generate_transaction()
+        service.send_transaction(transaction)
         sleep(3)
 
 
